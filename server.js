a63dafd4fc09a4cd17e41644b3242a2ed6c0986e
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-10-21 21:58:57 +0530

    adding port here

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const port = process.env.PORT || 9000;
 
 app.use(express.urlencoded({ extended: true }));
 
